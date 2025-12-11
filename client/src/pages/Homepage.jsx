84c5751079c007ede5c6954f28b023c08aa58cde
mehrnaz <pishyar.mehrnaz@gmail.com>
2025-12-11 09:50:42 +0100

    Featured & BlurCircle component was added

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
+import Featured from '../components/Featured';
+
 
 const Homepage = () => {
   return (
     <div>
         <HeroSection/>
+        <Featured/>
     </div>
   )
 }
