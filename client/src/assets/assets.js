80d62596c0de6680b8f41add22c0a36f6ab23317
mehrnaz <pishyar.mehrnaz@gmail.com>
2025-12-11 15:58:03 +0100

    TrailerSection component was added

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -6,6 +6,25 @@ export const assets = {
     Disney
 }
 
+export const dummyTrailers = [
+    {
+        image: "https://img.youtube.com/vi/WpW36ldAqnM/maxresdefault.jpg",
+        videoUrl: 'https://www.youtube.com/watch?v=WpW36ldAqnM'
+    },
+    {
+        image: "https://img.youtube.com/vi/-sAOWhvheK8/maxresdefault.jpg",
+        videoUrl: 'https://www.youtube.com/watch?v=-sAOWhvheK8'
+    },
+    {
+        image: "https://img.youtube.com/vi/1pHDWnXmK7Y/maxresdefault.jpg",
+        videoUrl: 'https://www.youtube.com/watch?v=1pHDWnXmK7Y'
+    },
+    {
+        image: "https://img.youtube.com/vi/umiKiW4En9g/maxresdefault.jpg",
+        videoUrl: 'https://www.youtube.com/watch?v=umiKiW4En9g'
+    },
+]
+
 const dummyCastsData = [
     { "name": "Milla Jovovich", "profile_path": "https://image.tmdb.org/t/p/original/usWnHCzbADijULREZYSJ0qfM00y.jpg", },
     { "name": "Dave Bautista", "profile_path": "https://image.tmdb.org/t/p/original/snk6JiXOOoRjPtHU5VMoy6qbd32.jpg", },
--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
 import Featured from '../components/Featured';
+import TrailerSection from '../components/TrailerSection';
 
 
 const Homepage = () => {
@@ -8,6 +9,7 @@ const Homepage = () => {
     <div>
         <HeroSection/>
         <Featured/>
+        <TrailerSection/>
     </div>
   )
 }
